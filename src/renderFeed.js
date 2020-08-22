73901fe1d6c993f76cb163ae818dc74ef6421a04
gorushkin <gorushkin@gmail.com>
2020-08-22 09:37:35 +0300

    поправил немного рендер и шаблон

--- src/renderFeed.js
+++ src/renderFeed.js
@@ -1,18 +1,18 @@
+import { divide } from 'lodash';
+
 export default ({ feeds, items }) => {
-  const container = document.querySelector('.container');
+  const container = document.querySelector('.feeds');
   container.innerHTML = '';
-  const containerInner = document.createElement('div');
   feeds.forEach(({ title, id }) => {
     const feedTitle = document.createElement('h2');
     feedTitle.textContent = title;
-    containerInner.appendChild(feedTitle);
+    container.appendChild(feedTitle);
     items
       .filter((item) => item.feedId === id)
       .forEach((element) => {
         const node = document.createElement('div');
         node.innerHTML = `<a href="${element.link}">${element.title}</a>`;
-        containerInner.appendChild(node);
+        container.appendChild(node);
       });
   });
-  container.appendChild(containerInner);
 };
