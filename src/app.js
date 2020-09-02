01f74bc869a90f512c803d622761379a99af4181
gorushkin <gorushkin@gmail.com>
2020-09-02 17:32:57 +0300

    переделал валидатор

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import onChange from 'on-change';
 import i18next from 'i18next';
-import _ from 'lodash';
-import getData from './getData';
+// import _ from 'lodash';
+// import getData from './getData';
 import validateUrl from './validateUrl';
-import { renderFeeds, renderStatus } from './renderData';
-import changeFormStatus from './changeFormStatus';
+// import { renderFeeds, renderStatus } from './renderData';
+// import changeFormStatus from './changeFormStatus';
+// import getItems from './getItems';
 import { en } from './locales';
-import getItems from './getItems';
 
 const app = () => {
   const elements = {
@@ -17,100 +17,37 @@ const app = () => {
   };
 
   const state = {
-    feeds: [],
+    feeds: [{ url: 'https://mobile-review.com/rss.xml' }],
     items: [],
-    updateStatus: false,
-    form: {
-      value: '',
-      isValid: null,
-      error: '',
-      message: '',
-      isFormBlocked: false,
-    },
+    status: '',
   };
 
-  const watchedState = onChange(state, (path) => {
+  const watchedState = onChange(state, (path, value) => {
     switch (path) {
-      case 'form.value': {
-        validateUrl(watchedState).then((result) => {
-          watchedState.form.error = result;
-        });
-        break;
-      }
-      case 'form.isFormBlocked': {
-        const { isFormBlocked } = watchedState.form;
-        changeFormStatus(elements, isFormBlocked);
-        break;
-      }
-      case 'form.error': {
-        const { error } = watchedState.form;
-        watchedState.form.isValid = !error;
-        if (error) watchedState.form.message = i18next.t(error);
-        break;
-      }
-      case 'form.message': {
-        renderStatus(watchedState, elements);
-        break;
-      }
-      case 'form.isValid': {
-        if (watchedState.form.isValid) {
-          const { value } = watchedState.form;
-          const { feeds } = watchedState;
-          watchedState.feeds = [{ url: value }, ...feeds];
-        }
-        break;
-      }
-      case 'feeds': {
-        watchedState.form.isFormBlocked = true;
-        const { value: url } = watchedState.form;
-        getData(url).then((data) => {
-          watchedState.form.message = i18next.t('loaded');
-          elements.form.reset();
-          onChange.target(watchedState).form.value = '';
-          onChange.target(watchedState).form.isValid = undefined;
-          onChange.target(watchedState).form.error = undefined;
-          watchedState.form.isFormBlocked = false;
-          const [feed, items] = getItems(watchedState.feeds, data, url);
-          const feedIndex = _.findIndex(watchedState.feeds, { url });
-          onChange.target(watchedState).feeds[feedIndex] = feed;
-          watchedState.items = [...items, ...watchedState.items];
-        });
-        break;
-      }
-      case 'items': {
-        const { items, feeds } = watchedState;
-        renderFeeds(feeds, items);
-        watchedState.updateStatus = true;
-        break;
-      }
-      case 'updateStatus': {
-        const { feeds } = watchedState;
-        if (watchedState.updateStatus) {
-          setTimeout(() => {
-            feeds.forEach((feed) => {
-              const { url } = feed;
-              getData(url).then((data) => {
-                const [updatedFeed, items] = getItems(feeds, data, url);
-                const feedIndex = _.findIndex(watchedState.feeds, { url });
-                onChange.target(watchedState).feeds[feedIndex] = updatedFeed;
-                watchedState.items = [...items, ...watchedState.items];
-              });
-            });
-            watchedState.updateStatus = false;
-          }, 5000);
-        }
+      case 'status': {
+        console.log('status: ', value);
         break;
       }
       default: {
-        throw new Error(`Unknown order state: '${path}'!`);
+        console.log(`Unknown order state: '${path}'!`);
       }
     }
   });
 
+  const formHandler = (url) => {
+    validateUrl(url, state.feeds).then((result) => {
+      if (url === result) {
+        console.log('ураааааа!!!');
+        watchedState.status = 'submit';
+      }
+    });
+  };
+
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    watchedState.form.value = formData.get('url');
+    const url = formData.get('url');
+    formHandler(url);
   });
 };
 
--- src/validateUrl.js
+++ src/validateUrl.js
@@ -1,16 +1,11 @@
 import * as yup from 'yup';
 import _ from 'lodash';
 
-const checkIfUrlExistInFeedsList = (feeds, url) => _.findIndex(feeds, { url }) !== -1;
-
-export default (watchedState) => {
-  const { feeds, form } = watchedState;
-  const { value: url } = form;
-  const schema = yup.string().url();
-  const isUrlExistInFeedsList = checkIfUrlExistInFeedsList(feeds, url);
-  return schema.isValid(url).then((result) => {
-    if (isUrlExistInFeedsList) return 'exist';
-    if (result) return null;
-    return 'invalid';
-  });
+export default (url, feeds) => {
+  const urlList = feeds.map((feed) => feed.url);
+  const schema = yup.string().url('is not valid').notOneOf(urlList, 'exist');
+  return schema
+    .validate(url)
+    .then((result) => result)
+    .catch((error) => error.message);
 };
